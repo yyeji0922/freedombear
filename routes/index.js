d4fccec22733b4012beae1627a55eb52fdcbcaf6
ilikefruit <ilikefruit@naver.com>
2017-08-01 00:33:11 +0900

    db 연결 추가했!

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+/*DB Connection*/
+var con = mysql.createConnection({
+	host : '10.10.10.10',
+	port : 3306,
+	user : 'freedombear',
+	password:'freedombear',
+	database:'freedombear'
+});
+
+con.connect();
+con.query('SELECT now()',function(err,rows,fields){
+	if (err) throw err;
+	console.log(rows);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
