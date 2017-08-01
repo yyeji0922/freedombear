bcb841df0da67962598e5e8a0e26c504c7467a71
psy2 <sogehao@gmail.com>
2017-08-01 12:09:59 +0000

    주석 지운거

--- routes/med.js
+++ routes/med.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-/*DB Connection*/
-var con = mysql.createConnection({
-	host : '10.10.10.10',
-	port : 3306,
-	user : 'freedombear',
-	password:'freedombear',
-	database:'freedombear'
-});
+// var mysql = require('mysql');
+// /*DB Connection*/
+// var con = mysql.createConnection({
+// 	host : '10.10.10.10',
+// 	port : 3306,
+// 	user : 'freedombear',
+// 	password:'freedombear',
+// 	database:'freedombear'
+// });
 
-con.connect();
-con.query('SELECT now()',function(err,rows,fields){
-	if (err) throw err;
-	console.log(rows);
-});
+// con.connect();
+// con.query('SELECT now()',function(err,rows,fields){
+// 	if (err) throw err;
+// 	console.log(rows);
+// });
 
 /* GET news listing. */
 router.get('/med', function(req, res) {
