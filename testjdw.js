f20b3778a1dd62279fea4f2d542758b7d0cb53f0
ilikefruit <enwide96@gmail.com>
2017-08-03 23:14:51 +0900

    쏘리 다시 올림ㅋㅋㅋㅋ

--- testjdw.js
+++ testjdw.js
@@ -6,8 +6,8 @@ var app = express();
 
 var connection = mysql.createConnection({
 	host: 'localhost',
-	user: 'freedombear',
-	password : 'freedombear',
+	user: 'root',
+	password : '',
 	database: 'freedombear'
 });
 
