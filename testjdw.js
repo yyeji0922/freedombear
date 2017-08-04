1a1adac8b90f48781922a1d702a520f9b178ac51
ilikefruit <enwide96@gmail.com>
2017-08-05 01:32:32 +0900

    db test

--- testjdw.js
+++ testjdw.js
@@ -1,21 +1,24 @@
+
 // get mongoose package
 var mongoose = require('mongoose');
 
 // connect to MongoDB / the name of DB is set to 'myDB'
-mongoose.connect('mongodb://'+config.account.mongoid+':'+config.account.mongopw+'@ds129030.mlab.com:29030/freedombear');
-
+//mongoose.connect('mongodb://'+config.account.mongoid+':'+config.account.mongopw+'@ds129030.mlab.com:29030/freedombear');
+mongoose.connect('mongodb://freedombear:freedombear@ds129030.mlab.com:29030/freedombear');
 // we get the pending connection to myDB running on localhost
 var db = mongoose.connection;
-// we get notified if error occurs
-db.on('error', console.error.bind(console, 'connection error:'));
 // executed when the connection opens
-db.once('open', function callback () {
+db.once('open', function () {
 	// add your code here when opening
-  	console.log("open");
+  	console.log("db connection ok");
+});
+// we get notified if error occurs
+db.on('error', function(err){
+	console.log("db error : ",err);
 });
 
 var Schema = mongoose.Schema;
-
+/*
 var userSchema= new Schema({
 	id: {type: String, required: true, unique: true },
 	password: {type:String, required: true },
@@ -26,24 +29,10 @@ var userSchema= new Schema({
 	image: {type:String, required:true, default: "default.jpg"},
 });
 
+*/
 
-var medSchema= new Schema({
-	med_id:{type:Number,required:true, unique:true},
-	title : {type: String, required:true },
-	content : {type: String, required:true },
-	writer_id : {type: String, required:true },
-	email : {type: String, required:true},
-	due_date : {type: Date},
-	pay: {type: Number, required: true, default: -1},
-	finished: {type: Boolean, required: true, default: false},
-	upload_time : { type: Date, required: true, default: Date.now },
-	summary : {type: String, required:true, default: "상세정보를 위해서 클릭하세요" },
-});
-
-
-var User = mongoose.model('User', userSchema);
-var Med = mongoose.model('Med',medSchema);
-
+//var User = mongoose.model('User', userSchema); 
+/*
 var user1 = new User({ id: 'yyeji0922', 
 			password: 'yyeji0922',
 			name:'에지',
@@ -70,34 +59,54 @@ user2.save(function(err,user1){
 });
 
 var med1 = new Med({
+	med_id:1,
 	title:'인물 사진 데이터 정형화 해주실분 구합니다.',
 	content:'안녕하세요 저희가 얼굴인식기능을 구현하기 위해 데이터를 정형화 해주실 분을 구합니다. 관심 있으신 분은 저희에게 연락해주세요.',
 	writer_id:'ilikefruit',
 	email:'freedombear@naver.com',
 	due_date:Date.now + 1000*60*24*3,
-	pay:1
-	finished: default,
-	upload_time:default,
+	pay:1,
+	summary:'인물 사진 데이터 정형화 알바 구합니다.'
+});
+*/
+/*med1.save(function(err,med1){
+	if(err)
+		console.log("error");
+});*/
+
+var Schema = mongoose.Schema;
+
+var medSchema= new Schema({
+	med_id:{type:Number,required:true, unique:true},
+	title : {type: String, required:true },
+	content : {type: String, required:true },
+	writer_id : {type: String, required:true },
+	email : {type: String, required:true},
+	due_date : {type: Date},
+	pay: {type: Number, required: true, default: -1},
+	finished: {type: Boolean, required: true, default: false},
+	upload_time : { type: Date, required: true, default: Date.now },
+	summary : {type: String, required:true, default: "상세정보를 위해서 클릭하세요" },
+});
+
+
+var Med = mongoose.model('Med',medSchema);
+var mydate =  new Date(2017,08,20,18,00);
+var med1 = new Med({
+	med_id:1,
+	title:'인물 사진 데이터 정형화 해주실분 구합니다.',
+	content:'안녕하세요 저희가 얼굴인식기능을 구현하기 위해 데이터를 정형화 해주실 분을 구합니다. 관심 있으신 분은 저희에게 연락해주세요.',
+	writer_id:'ilikefruit',
+	email:'freedombear@naver.com',
+	due_date: mydate,
+	pay:1,
 	summary:'인물 사진 데이터 정형화 알바 구합니다.'
 });
 
-var med2 = new Med({
-	title:'꽃 사진 데이터 정형화',
-	content:'안녕하세요 꽃 종류를 인식하는 어플을 만들기위해 데이터 라벨링이 필요합니다. 이메일로 연락주세요.',
-	writer_id:'yyeji0922',
-	email:'yyeji0922@naver.com',
-	due_date:Date.now + 1000*60*24*4,
-	pay:1.5
-	finished: default,
-	upload_time:default,
-	summary:default
-});
 
-med1.save(function(err,user1){
+med1.save(function(err,med2){
 	if(err)
-		console.log("error");
-});
-med2.save(function(err,user1){
-	if(err)
-		console.log("error");
+		console.log("error:can't insert");
+	else
+		console.log("insertion success");
 });
