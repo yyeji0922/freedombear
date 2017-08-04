5bf7e9f25de2701bcc1545dbb96f5ef7e9702f3a
yyeji0922 <yyeji0922@hanmail.net>
2017-08-04 19:57:50 +0900

    Med 대충 짬

--- routes/med.js
+++ routes/med.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-/*var storage = multer.diskStorage({
-  destination: path.join(__dirname, '../public/upload/news'),
-  filename: function (req, file, cb) {
-    cb(null, "news"  + Date.now());
-  }
-});
-var upload = multer({ storage: storage });
-*/
+var Med= require("../models/Med");
 
 /* GET news listing. */
-router.get('/med', function(req, res) {
-    //if(err) return res.json({success:false, message:err}); 
-    res.render('med');
+router.get('/med', function(req, res,next) {
+    Med.find({}).sort('-due_date').exec( function(err,data){
+		if (err) return res.json({success: false, message: err});
+		res.render('med',{ data : data} );
+	});
 });
 
 /* serach & show */
@@ -35,50 +29,46 @@ router.get('/med/new', function(req, res) {
 //LOGIN!
 router.post('/med/new', /*upload.single('filetoupload'),*/ function (req, res) {
     console.log(req.body);
-    /*pool.getConnection(function(error, connection) {
-        if (error) {
-            console.log("getConnection error" + error);
-            res.status(500).send(error);
-        } else {
-            var sql_search = 'select music_id from musicmining.play_list where user_id = ? and music_id = ?';
-            connection.query(sql_search, [req.body.user_id, req.body.music_id], function(error, rows) {
-                if (error) {
-                    console.log("찾는거 실패"+error);
-                } else {
-                    console.log("SUCCESS");
-                }
-            });           
-        }
-    });*/
+
     res.redirect('/med');
     
 });
 
 /*news 보여주기*/
-router.get('/med/:post_id', function(req, res) {
-    //if(err) return res.json({success:false, message:err});
-    res.render('med_per');
-});
+router.get('/med/:id', function(req, res) {
+    Med.findById(req.params.id, function(err,content){
+        if (err) return res.json({success: false, message: err});
+        res.render('med_per', { data : content , showornot : 0 } );
+    });
+ });
 
 /*update 페이지로 고우*/
 //LOGIN
-router.get('/med/:post_id/update', function(req, res) {
-    //if(err) return res.json({success:false, message:err});
-    res.render('med_per');
+router.get('/med/:id/update', function(req, res) {
+    Med.findById(req.params.id, function(err,content){
+        if (err) return res.json({success: false, message: err});
+        res.render('med_per', { data : content , showornot : 1 } );
+    });    
 });
 
 /*update 페이지 update*/
 //LOGIN + 내꺼
-router.post('/med/:post_id/update', function(req, res) {
-    //if(err) return res.json({success:false, message:err});
-    res.redirect('back');
+router.post('/med/:id/update', function(req, res) {
+    var updated={title: req.body.title, content:req.body.content, contact:req.body.contact, email:req.body.email, due_date:req.body.due_date, pay:req.body.pay, finished:req.body.finished, summary:req.body.summary };
+	Med.findByIdAndUpdate(req.params.id, updated, function (err,content) {
+		if(err) return res.json({success: false, message: err});
+		res.redirect('back');
+	});
+
 });
 
 /*update 해서 지우기로함*/
 //LOGIN
-router.delete('/med/:post_id/update', function(req, res) {
-    //if(err) return res.json({success:false, message:err});
-    res.redirect('back');
+router.delete('/med/:id/update', function(req, res) {
+    News.findOneAndRemove({ _id : req.params.id }, function (err,user) {
+    	if(err) return res.json({success: false, message: err});
+      res.redirect('back');
+  });
 });
 
 module.exports = router;
