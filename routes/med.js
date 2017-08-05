47089c42f8fee0e403b45a237c099d469d548141
ilikefruit <enwide96@gmail.com>
2017-08-05 14:45:50 +0900

    .

--- routes/med.js
+++ routes/med.js
@@ -29,8 +29,12 @@ router.get('/med/new', function(req, res) {
 //LOGIN!
 router.post('/med/new', /*upload.single('filetoupload'),*/ function (req, res) {
     console.log(req.body);
-    var due_date = req.body.text;
+    var due_date = req.body.date;
+    console.log(due_date);
     var strDate = due_date.split('/');
+    console.log(strDate[0]);
+    console.log(strDate[1]);
+    console.log(strDate[2]);
     var mydate = (strDate[2],strDate[0],strDate[1],23,59);
     var med1 = new Med({
         med_id:Med.count()+1,
@@ -42,14 +46,15 @@ router.post('/med/new', /*upload.single('filetoupload'),*/ function (req, res) {
         pay:req.body.pay,
         summary:req.body.summernote
     });
+    console.log(med1);
     med1.save(function(err,med1){
         if(err)
             console.log("error:can't insert");
         else
             console.log("insertion success");
     });
+
     res.redirect('/med', {user:req.user});
-    
 });
 
 /*news 보여주기*/
