cbd282b3277e725675e7ed2e106fff8b3f002ee3
psy2 <sogehao@gmail.com>
2017-08-05 21:08:06 +0000

    새기능

--- public/js/plugins/jquery.bootstrap.wizard.js
+++ public/js/plugins/jquery.bootstrap.wizard.js
@@ -38,10 +38,11 @@ var bootstrapWizardCreate = function(element, options) {
 
 		// See if we're currently in the first/last then disable the previous and last buttons
 		$($settings.previousSelector, element).toggleClass('disabled', (obj.firstIndex() >= obj.currentIndex()));
+		$($settings.nextSelector, element).toggleClass('disabled', (obj.currentIndex() >= obj.navigationLength()));
 		$($settings.nextSelector, element).toggleClass('doregister', (obj.currentIndex() >= obj.navigationLength()));
 		
-		// $('.doregister').text("")
-		$('.doregister').append('<button class="btn btn-primary btn-custom waves-effect waves-light w-md" type="submit">Register</button>');
+		$('.lastlast').remove()
+		$('.doregister').append('<button class="btn btn-primary btn-custom waves-effect waves-light w-md lastlast" type="submit">Register</button>');
 
 
 		$($settings.nextSelector, element).toggleClass('hidden', (obj.currentIndex() >= obj.navigationLength() && $($settings.finishSelector, element).length > 0));
